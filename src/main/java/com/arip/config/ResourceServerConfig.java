6bf57d0aca0308e48cefde5142a02eb52cc5b218
Arip Hidayat <ariphidayat@hotmail.com>
2017-08-31 01:08:28 +0700

    add Web Security Config & InMemoryAuthentication

--- src/main/java/com/arip/config/ResourceServerConfig.java
+++ src/main/java/com/arip/config/ResourceServerConfig.java
@@ -1,8 +1,8 @@
-package com.arip.config;
+ package com.arip.config;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+ import org.springframework.context.annotation.Configuration;
+ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+ import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
 /**
  * Created by Arip Hidayat on 8/30/2017.
@@ -14,7 +14,6 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
     public void configure(HttpSecurity http) throws Exception {
         http
             .authorizeRequests()
-            .antMatchers( "/api/**").authenticated()
-            .anyRequest().permitAll();
+            .antMatchers( "/api/**").authenticated();
     }
 }
---
+++ src/main/java/com/arip/config/WebSecurityConfig.java
@@ -0,0 +1,24 @@
+package com.arip.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Created by Arip Hidayat on 8/30/2017.
+ */
+
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+            .inMemoryAuthentication()
+            .withUser("user").password("password_user").roles("USER")
+            .and().withUser("admin").password("password_user").roles("ADMIN")
+            .and().withUser("developer").password("password_developer").roles("DEVELOPER");
+    }
+}
