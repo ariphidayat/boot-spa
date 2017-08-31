7679c26e1a86f9ef9fa683b216d6d62a1e86ce73
Arip Hidayat <ariphidayat@hotmail.com>
2017-08-31 21:39:19 +0700

    Flyway

---
+++ src/main/resources/db/migration/V001__Create_Tables.sql
@@ -0,0 +1,15 @@
+create table users(
+  email varchar(50),
+  password varchar(50) not null,
+  enabled boolean not null,
+  constraint pk_users primary key (email)
+);
+
+create table authorities (
+  authority_id serial,
+  email varchar(50) not null,
+  authority varchar(50) not null,
+  constraint pk_authorities primary key(authority_id),
+  constraint fk_authorities_users foreign key(email) references users(email)
+);
+create unique index idx_authorities on authorities(email, authority);
---
+++ src/main/resources/db/migration/V002__Add_Datas.sql
@@ -0,0 +1,5 @@
+insert into users(email, password, enabled) values('arip@hidayat', 'ar1p', true);
+insert into users(email, password, enabled) values('alisiana@ulfah.com', '4lis', true);
+
+insert into authorities(authority_id, email, authority) values(1, 'arip@hidayat', 'DEVELOPER');
+insert into authorities(authority_id, email, authority) values(2, 'alisiana@ulfah.com', 'ADMIN');
