13ac4f99f068bae873213f0e394234fe668d07ff
Arip Hidayat <ariphidayat@hotmail.com>
2017-09-02 08:46:36 +0700

    Expose using @RestController

---
+++ src/main/java/com/arip/controller/UserController.java
@@ -0,0 +1,24 @@
+package com.arip.controller;
+
+import com.arip.model.User;
+import com.arip.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * Created by Arip Hidayat on 9/2/2017.
+ */
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/users")
+    public List<User> getUsers() {
+        return userRepository.findAll();
+    }
+}
