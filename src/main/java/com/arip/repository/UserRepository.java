4f96f1e26753e80a61531ba664f70b81463fd4bc
Arip Hidayat <ariphidayat@hotmail.com>
2017-09-03 09:45:18 +0700

    add  search parameter repository
    access example:
    http://localhost:8080/api/users/search/findByEmail{?email}

--- src/main/java/com/arip/repository/UserRepository.java
+++ src/main/java/com/arip/repository/UserRepository.java
@@ -2,11 +2,12 @@ package com.arip.repository;
 
 import com.arip.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 /**
  * Created by Arip Hidayat on 8/31/2017.
  */
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    User findByEmail(String email);
+    User findByEmail(@Param("email") String email);
 }
