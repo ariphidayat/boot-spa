7af24afd098aa1930d952cbc93d52f657245ab01
Arip Hidayat <ariphidayat@hotmail.com>
2017-09-08 22:39:09 +0700

    Spring Session JDBC : annotation based

---
+++ src/main/java/com/arip/config/HttpSessionConfig.java
@@ -0,0 +1,9 @@
+package com.arip.config;
+
+import org.springframework.session.jdbc.config.annotation.web.http.EnableJdbcHttpSession;
+
+/**
+ * Created by Arip Hidayat on 9/8/2017.
+ */
+@EnableJdbcHttpSession
+public class HttpSessionConfig {}
