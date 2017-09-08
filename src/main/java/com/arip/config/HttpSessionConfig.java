64aad8c378f287f6d62772e9d000409150edc3da
Arip Hidayat <ariphidayat@hotmail.com>
2017-09-09 00:09:39 +0700

    Spring Session JDBC + Cookies Http Session Strategy

--- src/main/java/com/arip/config/HttpSessionConfig.java
+++ src/main/java/com/arip/config/HttpSessionConfig.java
@@ -1,17 +1,9 @@
 package com.arip.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.session.jdbc.config.annotation.web.http.EnableJdbcHttpSession;
-import org.springframework.session.web.http.HeaderHttpSessionStrategy;
 
 /**
  * Created by Arip Hidayat on 9/8/2017.
  */
 @EnableJdbcHttpSession
-public class HttpSessionConfig {
-
-    @Bean
-    public HeaderHttpSessionStrategy strategy() {
-        return new HeaderHttpSessionStrategy();
-    }
-}
+public class HttpSessionConfig {}
